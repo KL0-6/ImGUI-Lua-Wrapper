02ea0ecbc2e446c3853b1022b684854a8f34f57b
ShowerHead <145412851+KL0-6@users.noreply.github.com>
2024-07-12 00:15:29 -0400

    Added OSX compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,12 @@ int main()
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 #if defined(__APPLE__)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#elif
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 #endif
 
     GLFWmonitor* monitor = glfwGetPrimaryMonitor();
